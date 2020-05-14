32e15b7b188cbe35533b99cfce9734d47f549000
Josh Franco <jsfranco127@gmail.com>
2020-05-14 14:31:43 -0700

    Added Xcode Proj to the git

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Josh Franco on 5/14/20.
+//  Copyright © 2020 Josh Franco. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
